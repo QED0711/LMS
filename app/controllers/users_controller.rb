c00cc998ededaa92ea36b945937ab32cd4c03ea1
QED0711 <qdizon@gmail.com>
2018-09-17 15:08:34 -0400

    added basic user signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    if params[:user][:password] != params[:user][:password_confirm]
+      @user = User.new(user_params)
+      render :new, alert: "Passwords did not match"
+    end
+    user = User.create(user_params)
+    session[:user_id] = user.id
+    redirect_to user_path(user)
+    # binding.pry
   end
 
   def show
+    @user = current_user
   end
 
   private
