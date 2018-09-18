b703cca4c1875416ebeafc6f7ed59617b8fe3ae0
qed0711 <qdizon@gmail.com>
2018-09-18 16:43:06 -0400

    only email and password needed to login. added home button to navigation banner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def sign_in
-    user = User.find_by(name: params[:user][:name], email: params[:user][:email])
+    user = User.find_by(email: params[:user][:email])
     if !!user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       redirect_to current_user
