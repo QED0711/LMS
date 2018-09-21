bceaabd1cf8f3d4b9862a5920c486792609d7648
QED0711 <qdizon@gmail.com>
2018-09-21 08:19:41 -0400

    category index and show pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,12 @@
 class CategoriesController < ApplicationController
   
   def index
-    @categories = Categories.all
+    @categories = Category.all
   end
 
   def show
     @category = Category.find(params[:id])
+    @lessons = @category.lessons
   end
-  
+
 end
